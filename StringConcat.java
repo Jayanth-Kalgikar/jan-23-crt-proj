b809e63474b86e2b5c89a7d021b11ea7b419f99e
Jayanth Kalgikar <jayanthkalgikar2005@gmail.com>
2025-01-25 16:12:50 +0530

    second commit

---
+++ StringConcat.java
@@ -0,0 +1,14 @@
+public class StringConcat {
+    public static void main(String[] args) {
+
+        String str1 = ("Add this and--->");
+        String st2 = ("THISSSS!!");
+        String str3;
+         str3 = str1.concat(st2);
+         
+         System.out.println( str3);
+         
+        
+
+    }
+}
